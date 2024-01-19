49040743577dbe9da373a299762563f24411a385
HANGYESUK <akakakak66@naver.com>
2024-01-19 19:37:25 +0900

    Feature: navigation바 추가

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,3 @@
-import { auth } from '../firebase.ts';
-import { useNavigate } from 'react-router-dom';
-
 export default function Home() {
-  const navigate = useNavigate();
-  const onLogOut = () => {
-    auth.signOut().then(() => navigate('/login'));
-  };
-  return (
-    <h1>
-      <button onClick={onLogOut}>Log Out</button>
-    </h1>
-  );
+  return <h1>Home</h1>;
 }
