1f5c44ed5df51a7bafb7762755a967b5928bf3c8
HANGYESUK <akakakak66@naver.com>
2024-01-23 19:22:20 +0900

    Fix: Create Home
    - TimeLine Component 추가

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,19 @@
 import PostTweetForm from '../components/PostTweetForm.tsx';
+import styled from 'styled-components';
+import Timeline from '../components/Timeline.tsx';
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-auto-rows: 1fr 5fr;
+`;
 
 export default function Home() {
   return (
-    <>
+    <Wrapper>
       <PostTweetForm />
-    </>
+      <Timeline />
+    </Wrapper>
   );
 }
