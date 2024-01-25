12258ace295665645b29605bea03ec0d28e6244b
HANGYESUK <akakakak66@naver.com>
2024-01-25 23:07:39 +0900

    Fix: Update Home
    - display grid => flex 변경

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 import Timeline from '../components/Timeline.tsx';
 
 const Wrapper = styled.div`
-  display: grid;
-  gap: 50px;
+  display: flex;
+  flex-direction: column;
   overflow-y: scroll;
-  grid-auto-rows: 1fr 5fr;
 `;
 
 export default function Home() {
